3b074376173373171ca448d6e8dac32e9aac4095
Jordan McClintock <jmac3293@gmail.com>
2021-05-31 12:18:55 -0500

    Removed duplicate code and updated references to contact page.

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@
 	<div class="navigation">
 		<ul class="container">
 			<li><a href="projects.html" class="removeStyling">Projects</a></li>
-			<li><a href="contact.html" class="removeStyling">Contact</a></li>
+			<li><a href="contact.php" class="removeStyling">Contact</a></li>
 			<li><a href="https://www.jordanmcclintock.com" class="removeStyling">About</a></li>
     	</ul>
 	</div>
